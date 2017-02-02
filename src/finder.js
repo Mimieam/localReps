598b0a4680a7c549ce66d5b86e3a28989ef985a0
Mimieam <t.echismimi@gmail.com>
2017-02-02 13:01:36 -0600

    async folder search implemented

--- src/finder.js
+++ src/finder.js
@@ -7,44 +7,69 @@ const EXCLUDES = [
   "bower_components",
   "npm",
   ".meteor",
-  ".Trash"
+  ".Trash",
+  ".cache",
 ]
 
 const findDirectories = (p) => {
-  let dirs = []
-  // returns all directories within a search path p
-  try{
-    dirs = fs.readdirSync(p)
-      .filter(
-        f => { return fs.existsSync(p) && fs.lstatSync(p+"/"+f).isDirectory()}
-      )
-  } catch (e) {
-    console.log(e.name +  " => " + e.message)
-  }
+      return new Promise((resolve, reject) => {
+        fs.readdir(p, (err, items) => {
+          if (err) reject(err)
+          // console.log(p)
+          let dirs = !items ? [] : items.filter( f => { return fs.existsSync(p) && fs.lstatSync(p+"/"+f).isDirectory()})
+          return resolve(dirs)
+        })
+      })
+}
+
+const createNewSearchPaths = (currentLocation, dirs) => {
   return dirs
+          .filter(subfolder => !EXCLUDES.includes(subfolder))
+          .map(subfolder => currentLocation + "/" + subfolder)
 }
 
-const _bfs = (searchPath) => {
-  let res = []
-  let directories = findDirectories(searchPath)
-  if (directories.includes(".git")) {
-    res.push(searchPath)
-    return res
-  }
-  else {
-    directories.forEach ((subfolder) => {
-      if (!EXCLUDES.includes(subfolder)){
-        res.push(..._bfs(searchPath + "/" + subfolder))
-      }
-    });
-  }
+let res = []
+const _bfs = (searchPath, arrayOfPromises) => {
+  let directories = []
+  let p = findDirectories(searchPath)
+      .then((directories) => {
+          if (directories.includes(".git")) {
+            res.push(searchPath)
+            console.log("............", searchPath)
+            return (res)
+          }
+          else {
+            let subfolders = createNewSearchPaths(searchPath, directories)
+            for (let _path of subfolders){
+                // console.log(_path , arrayOfPromises)
 
-  return res
+               // return (Promise.resolve().then( () => _bfs(_path)))
+               let arr = _bfs(_path, [])
+               arrayOfPromises.push(arr)
+            }
+            return Promise.all(arrayOfPromises)
+          }
+      })
+  return (p)
 }
 
- const bfs = (searchPath) => {
+ const bfs = async (searchPath) => {
     // search the provided path for a .git folder
-  return _bfs(searchPath)
+  let proms = []
+  const p = await _bfs(searchPath, proms).then(async (data)=> {
+   // console.log(data)
+    await Promise.all(proms).then((data)=>{
+      console.log(res)
+    // eval('let arr = ['+data.toString()+']');
+    // console.log(arr);
+      // console.log(data[2])
+    }).catch((e)=>{console.log(e)})
+  })
+
+  // await Promise.all(fp).then((data)=> console.log(fp))
+   console.log("[------------------------------------------\
+---------------------------------------------\
+----------------------------->>>>" )
 }
 
 // patiently awaiting Node support for es6 modules...
@@ -52,4 +77,7 @@ module.exports = {
     bfs: bfs,
 }
 
-console.log(bfs('/Users/Miezan/'))
+bfs('/Users/Miezan/Desktop')
+  // .then(data=>console.log(data))
+  // .catch((e)=>{console.log(e)})
+
